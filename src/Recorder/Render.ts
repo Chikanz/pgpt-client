3cfa845f5098040212e86772d1f37e936161feb2
Zac Lucarelli <zac.lucarelli@outlook.com>
2023-08-30 19:05:19 +1000

    Stream screen recording to disk

--- src/Recorder/Render.ts
+++ src/Recorder/Render.ts
@@ -1,27 +1,32 @@
 import { ipcRenderer } from 'electron';
-import { writeFile, existsSync, mkdirSync } from 'fs';
+import { createWriteStream, existsSync, mkdirSync } from 'fs';
 import path from 'path';
 
 let mediaRecorder: MediaRecorder;
-let recordedChunks = [];
+let videoElement = document.querySelector('video');
 
-const videoElement = document.querySelector('video');
+const recordingPath = path.join(process.env.PORTABLE_EXECUTABLE_DIR! || __dirname, "/recordings/recording.webm");
+const recordingDir = path.dirname(recordingPath);
 
-//Start recording on document load
+if (!existsSync(recordingDir)) {
+    mkdirSync(recordingDir, { recursive: true });
+}
+
+const writable = createWriteStream(recordingPath);
+
+// Start recording on document load
 document.addEventListener('DOMContentLoaded', async () => {
     await startRecording();
 });
 
 async function startRecording() {
-    const inputSources: Electron.DesktopCapturerSource[] = await ipcRenderer.invoke('getSources')
+    const inputSources: Electron.DesktopCapturerSource[] = await ipcRenderer.invoke('getSources');
 
     //Get input where name is Screen 1
-    const screenSource = inputSources.find(source => source.name === 'Screen 1')
+    const screenSource = inputSources.find(source => source.name === 'Screen 1');
     const screenId = screenSource.id;
 
     const constraints = {
-        cursor: 'never',
-        
         audio: {
             mandatory: {
                 chromeMediaSource: 'desktop'
@@ -35,11 +40,9 @@ async function startRecording() {
         }
     };
 
-    // Create a Stream
     //@ts-ignore
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
 
-    // Preview the source in a video element
     videoElement.srcObject = stream;
     await videoElement.play();
 
@@ -52,42 +55,22 @@ async function startRecording() {
 
 function onDataAvailable(e) {
     console.log('Pushing chunky');
-    recordedChunks.push(e.data);
+    const blob = e.data;
+    blob.arrayBuffer().then((arrayBuffer) => {
+        writable.write(Buffer.from(arrayBuffer));
+    });
 }
 
-async function stopRecording() {
+function stopRecording() {
     if (mediaRecorder && mediaRecorder.state !== 'recording') {
         console.log("not recording m8");
         return;
     }
 
     mediaRecorder.stop();
-
     console.log("Stopping recording");
-    videoElement.srcObject = null
-
-    const blob = new Blob(recordedChunks, {
-        type: 'video/webm; codecs=vp9'
-    });
-
-    const buffer = Buffer.from(await blob.arrayBuffer());
-    recordedChunks = [];
-
-    const recordingPath = path.join(process.env.PORTABLE_EXECUTABLE_DIR! || __dirname, "/recordings/recording.webm");
-    //Make recording path if it doesn't exist
-    const recordingDir = path.dirname(recordingPath);
-    if (!existsSync(recordingDir)) {
-        mkdirSync(recordingDir, { recursive: true });
-    }
-
-    writeFile(recordingPath, buffer, (err) => {
-        if (err) {
-            console.error('Failed to save video ', err);
-        } else {
-            console.log('video saved successfully!');
-        }
-    });
-
+    videoElement.srcObject = null;
+    writable.end();
 }
 
 ipcRenderer.on('stop-recording', () => {
@@ -101,4 +84,3 @@ ipcRenderer.on('app-quitting', () => {
 window.addEventListener('beforeunload', () => {
     stopRecording();
 });
-
